5bc25616687af38c99b79b15c93a48644b7424fe
Patricio <patricio.adres.def@gmail.com>
2018-11-30 07:01:30 -0300

    intento 3

--- js/app.js
+++ js/app.js
@@ -1,14 +1,9 @@
-(function(){
-    var xhttp=new XMLHttpRequest();
-    var url="http://localhost:8000/PERROs/";
-    var txtData = document.getElementById("txtData"); 
-
-    xhttp.onreadystatechange=function(){
-        if(this.readyState==4 && this.status==200){
-            var data = this.responseText;
-            txtData.innerHTML=data;
-        }
-    }
-    xhttp.open("GET",url,true);
-    xhttp.send();
-})();
+( function() {
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker
+                 .register('./service-worker.js')
+                 .then(function() { 
+                     console.log('Service Worker Registered'); 
+                });
+      }
+} )( );
